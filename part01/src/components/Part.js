887ffcd1a32ee0b025b28fd7fc4ad771a5918440
Jan Fincke <jan.fincke@cadworks.fi>
2021-08-10 12:30:51 +0300

    Add first exercise solution

---
+++ part01/src/components/Part.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Part = (props) => {
+    return (
+        <>
+        <p>
+            {props.part} {props.exercise}
+        </p>
+        </>
+    )
+}
+
+export default Part
