f4a0afba219ef64a864b2cc5165cfb2483e59579
Jan Fincke <jan.fincke@cadworks.fi>
2021-08-10 14:26:43 +0300

    Remove redundant line

--- part01/src/components/Total.js
+++ part01/src/components/Total.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const Total = ({arrayExercisesCount}) => {
-    //let exercises_counts = parts.map(x => x.exercises)
-
     return (
         <>
             <p>Number of exercises {arrayExercisesCount.reduce((x, y) => x + y, 0)}</p>
